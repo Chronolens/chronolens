a7027bc5698303be71ddd4cffd55339773bf4d2a
lucasverdelho <PG50221@alunos.uminho.pt>
2024-11-20 12:10:34 +0000

    writing request to the correct subjects

--- api/src/routes/clip_search.rs
+++ api/src/routes/clip_search.rs
@@ -4,16 +4,14 @@ use axum::{
     Json,
 };
 use http::StatusCode;
+use tokio_util::bytes::Bytes; 
 use crate::{ServerConfig, models::api_models::{PreviewItem, SearchQuery}};
-
-
 
 pub async fn clip_search(
     State(server_config): State<ServerConfig>, 
     Extension(user_id): Extension<String>,    
     Query(params): Query<SearchQuery>,        
 ) -> Response {
-
     let query = params.query;
     let page = params.page.unwrap_or(1).max(1); 
     let page_size = params.page_size.unwrap_or(10).clamp(1, 30); 
@@ -21,17 +19,20 @@ pub async fn clip_search(
     if query.is_empty() {
         return (StatusCode::BAD_REQUEST, "Query is required").into_response(); 
     }
-   
-    let response_data = vec![
-        PreviewItem {
-            id: format!("item-{}-1", page),
-            preview_url: format!("https://example.com/preview/{}-1", page),
-        },
-        PreviewItem {
-            id: format!("item-{}-2", page),
-            preview_url: format!("https://example.com/preview/{}-2", page),
-        },
-    ];
-   
-    (StatusCode::OK, Json(response_data)).into_response()
+
+    
+    let query_bytes = Bytes::from(query.clone()); 
+
+    
+    match server_config
+        .nats_jetstream
+        .publish("clip-process", query_bytes) 
+        .await
+    {
+        Ok(_) => (), 
+        Err(..) => return StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+    };
+
+    
+    (StatusCode::OK, Json("Query sent successfully")).into_response()
 }
