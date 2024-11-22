77a00fc81e0ed3f6fd560ea2a01059c17f57fb8d
afonsofrancof <afonso@francof.net>
2024-11-22 13:50:26 +0000

    Fix database migrations and previews return list

--- database/migration/src/m003_media.rs
+++ database/migration/src/m003_media.rs
@@ -25,6 +25,25 @@ impl MigrationTrait for Migration {
                     .col(big_integer(Media::CreatedAt))
                     .col(big_integer(Media::LastModifiedAt))
                     .col(boolean(Media::Deleted))
+                    .col(big_integer(Media::FileSize))
+                    .col(string(Media::FileName))
+                    .col(double_null(Media::Longitude))
+                    .col(double_null(Media::Latitude))
+                    .col(integer_null(Media::ImageWidth))
+                    .col(integer_null(Media::ImageLength))
+                    .col(string_null(Media::Make))
+                    .col(string_null(Media::Model))
+                    .col(string_null(Media::Fnumber))
+                    .col(string_null(Media::ExposureTime))
+                    .col(string_null(Media::PhotographicSensitivity))
+                    .col(integer_null(Media::Orientation))
+                    .col(
+                        ColumnDef::new_with_type(
+                            Media::ClipEmbeddings,
+                            ColumnType::Vector(Some(512)),
+                        )
+                        .null(),
+                    )
                     .to_owned(),
             )
             .await
@@ -47,4 +66,17 @@ pub enum Media {
     CreatedAt,
     LastModifiedAt,
     Deleted,
+    FileSize,
+    FileName,
+    Longitude,
+    Latitude,
+    ImageWidth,
+    ImageLength,
+    Make,
+    Model,
+    Fnumber,
+    ExposureTime,
+    PhotographicSensitivity,
+    Orientation,
+    ClipEmbeddings,
 }
